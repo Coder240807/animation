b048b08bfdc28eeb525421d2cd660152e810d475
Aakarsh Singh Chauhan <aakarshsingh8528@gmail.com>
2026-09-05 09:27:02 +0530

    Add responsive background

--- main.js
+++ main.js
@@ -1,12 +1,18 @@
 let canvas = document.getElementById('canvas');
-
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-
 let ctx = canvas.getContext('2d');
 
-ctx.fillStyle = 'skyblue';
-ctx.fillRect(0, 0, canvas.width, 550);
+function drawScene() {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
 
-ctx.fillStyle = '#3d251e';
-ctx.fillRect(0, 550, canvas.width, canvas.height - 550);
+    ctx.fillStyle = 'skyblue';
+    ctx.fillRect(0, 0, canvas.width, canvas.height * 0.65);
+
+    ctx.fillStyle = '#3d251e';
+    ctx.fillRect(0, canvas.height * 0.65, canvas.width, canvas.height * 0.35);
+
+}
+
+drawScene();
+
+window.addEventListener('resize', drawScene);
